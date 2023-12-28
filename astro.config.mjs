d32138b8b43e8b2ce86b57e7afa33353aa6687fd
Udit <23015406+udit-001@users.noreply.github.com>
2023-12-28 15:54:10 +0530

    fix(deploy): Bypass default form submission behavior

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,4 @@ import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({
-  output: "hybrid",
-  adapter: netlify()
-});
+export default defineConfig({});
