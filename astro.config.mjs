95bc92727b9a448b5b71bb1c80e57c3d12acb887
Udit <23015406+udit-001@users.noreply.github.com>
2023-12-28 15:40:24 +0530

    fix(deploy): Update project config to include netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    output: "hybrid"
+  output: "hybrid",
+  adapter: netlify()
 });
