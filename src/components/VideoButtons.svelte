b58a884cc04f9e016972804fca84675fe1ecfc49
Udit <23015406+udit-001@users.noreply.github.com>
2023-12-30 07:23:50 +0530

    fix(ui): resolve broken video buttons on smaller screens

--- src/components/VideoButtons.svelte
+++ src/components/VideoButtons.svelte
@@ -1,6 +1,6 @@
 <div class="row mt-3">
     <div class="clearfix">
-        <button type="button" class="btn btn-primary btn-lg w-25 float-start" on:click={goPrev} class:d-none={$activeChildIndex === 0}>
+        <button type="button" class="btn btn-primary btn-lg w-sm-25 float-start" on:click={goPrev} class:d-none={$activeChildIndex === 0}>
             <i class="fa-solid fa-chevron-left"></i> Previous
         </button>
         <button type="button" class="ms-auto btn btn-primary btn-lg w-25 float-end" on:click={goNext} class:d-none={($activeChildIndex === ($lessons.length - 1)) || loading}>
