ca032001cd699378f062f1fa96cd50e703a50774
Udit <23015406+udit-001@users.noreply.github.com>
2023-12-31 02:18:31 +0530

    feat(deploy): Add SSR mode with Netlify integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  output: 'server',
+  integrations: [svelte()],
+  adapter: netlify()
 });
