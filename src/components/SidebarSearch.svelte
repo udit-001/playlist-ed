c233667d5b7b4062d32d0f85871f41ed90e59b63
Udit <23015406+udit-001@users.noreply.github.com>
2024-01-04 10:02:32 +0530

    feat(sidebarSearch): Implement dynamic clear icon in search input

--- src/components/SidebarSearch.svelte
+++ src/components/SidebarSearch.svelte
@@ -1,9 +1,17 @@
 <div class="filter-docs sticky-top p-3">
-    <input type="text" class="form-control" placeholder="Search" bind:value={$sidebarQuery}>
+    <div class:input-group={$sidebarQuery !== ""}>
+    <input type="search" class="form-control" placeholder="Search" aria-label="Search" bind:value={$sidebarQuery}>
+    <button type="button" class="btn btn-secondary" aria-label="Close" on:click={clearQuery} aria-expanded="false" class:d-none={$sidebarQuery === ""} class:rounded={$sidebarQuery === ""}>
+        <i class="bi bi-x"></i>
+    </button>
+    </div>
 </div>
 
 <script>
     import { sidebarQuery } from "../store/state";
+    function clearQuery(){
+        $sidebarQuery = "";
+    }
 </script>
 
 <style>
