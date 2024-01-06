58630b53068a3c289f1c8db10cb5dd7d816c63d8
Udit <23015406+udit-001@users.noreply.github.com>
2024-01-06 07:28:38 +0530

    chore: remove unused store file

--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { atom } from 'nanostores'
-
-export const $videoLink = atom('');
-
-export const exampleClicked = atom(0);
-export const playlistLink = atom('');
-
-export const lessons = atom([]);
-export const activeChildIndex = atom(0);
