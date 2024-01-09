f265ee5fbd318d609f21c2232082dfc6d3ef57d3
Udit <23015406+udit-001@users.noreply.github.com>
2024-01-10 02:04:22 +0530

    feat(header): Add app icon and update app name

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,19 +7,24 @@
     <button class:d-none={videoId === undefined} class="navbar-toggler d-md-none" type="button" data-bs-toggle="offcanvas" aria-label="Close" data-bs-target="#sidebar-example">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand mx-auto w-75 text-center text-truncate" href="#" class:d-none={loading}>
+    <span class="navbar-brand mx-auto w-75 text-center text-truncate" href="#" class:d-none={loading}>
+        {#if logo}
+            <HeaderLogo />
+        {/if}
         {title}
-    </a>
+    </span>
     <span class="placeholder col-5 mx-auto m-2 placeholder-lg placeholder-wave" class:d-none={!loading}></span>
     </div>
 </nav>
 
 <script>
     import { lessons } from "../store/state.js";
+    import HeaderLogo from "./HeaderLogo.svelte";
     export var loading = false;
     export var videoId;
+    export var logo = false;
 
-    let title = loading === true ? 'Loading...': 'YT Playlist Zen';
+    let title = loading === true ? 'Loading...': 'Playlist-Ed';
 
     $: {
         if(videoId !== undefined){
