166f329f60ab6339187d91b470164bf3352de5d2
Udit <23015406+udit-001@users.noreply.github.com>
2024-01-12 06:14:01 +0530

    feat(middleware): implement default metadata on API failure

--- src/middleware.js
+++ src/middleware.js
@@ -35,25 +35,43 @@ export async function fetchInvidiousInstances(){
             }
             return filteredUrls;
         }
-    )
-    return response;
+    ).catch(error => null)
+    if(response){
+        return response;
+    }
+    else{
+        return null;
+    }
   }
 
 
 export async function fetchVideoDetails(videoId){
     let instances = await fetchInvidiousInstances();
-    let apiInstances = instances.filter(i => i['api'] === true);
-    var baseUrl = apiInstances[0]['uri'];
-    const url = baseUrl + "/api/v1/videos/" + videoId;
-    const data = await fetch(url).then(response => {
-        if(!response.ok){
-            throw new Error(`HTTP Error! Status: ${response.status}`);
+    if(instances !== null){
+        let apiInstances = instances.filter(i => i['api'] === true);
+        var baseUrl = apiInstances[0]['uri'];
+        const url = baseUrl + "/api/v1/videos/" + videoId;
+        const data = await fetch(url).then(response => {
+            if(!response.ok){
+                return null;
+            }
+            return response.json()
+        }).catch((e)  => {
+            console.log(e);
+        })
+        if (data){
+            return {
+                title : data["title"],
+                thumbnail: data["videoThumbnails"][0]["url"]
+            }
         }
-        return response.json()
-    })
-    return {
-        title : data["title"],
-        thumbnail: data["videoThumbnails"][0]["url"]
+        else{
+            return null
+        }
+
+    }
+    else{
+        return null;
     }
 }
 
@@ -61,15 +79,28 @@ export const onRequest = async (context, next) => {
     if (context.url.pathname.includes("lessons") === true) {
         var videoId  = context.url.pathname.split("/")[3];
         var videoData = await fetchVideoDetails(videoId);
-
-        const response = await next();
-        const html = await response.text();
-        const title = videoData.title + " | Playlist-Ed";
-        var updatedHtml = html.replaceAll("META TITLE", title).replaceAll("META IMAGE", videoData.thumbnail).replaceAll("META DESCRIPTION", "View on Playlist-Ed")
-        return new Response(updatedHtml, {
-            status: 200,
-            headers: response.headers
-        });
-      }
-      return next();
+        if(videoData !== null){
+            const response = await next();
+            const html = await response.text();
+            const title = videoData.title + " | Playlist-Ed";
+            var updatedHtml = html.replaceAll("META TITLE", title).replaceAll("META IMAGE", videoData.thumbnail).replaceAll("META DESCRIPTION", "View on Playlist-Ed")
+            return new Response(updatedHtml, {
+                status: 200,
+                headers: response.headers
+            });
+        }
+        else{
+            const response = await next();
+            const html = await response.text();
+            var title = "Playlist-Ed: Your solution to chaotic study sessions";
+            var thumbnail = "/images/playlist-ed-banner.png";
+            var description = "Streamline your study sessions by curating clutter-free, uninterrupted playlists for your focused learning.";
+            var updatedHtml = html.replaceAll("META TITLE", title).replaceAll("META IMAGE", thumbnail).replaceAll("META DESCRIPTION", description)
+            return new Response(updatedHtml, {
+                status: 200,
+                headers: response.headers
+            });
+        }
+    }
+    return next();
     };
