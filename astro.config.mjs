36f988c7bfa86e63b15932f7b15c8860cb056864
Udit <23015406+udit-001@users.noreply.github.com>
2024-01-27 03:55:26 +0530

    feat: add sentry integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,18 @@ import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import netlify from "@astrojs/netlify";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [svelte()],
+  integrations: [svelte(), sentry({
+      dsn: "https://f7358c6bd68d9fcf222f023f9a1fedc1@o444924.ingest.sentry.io/4506639903096832",
+      sourceMapsUploadOptions: {
+        project: "playlist-ed",
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+      },
+    })],
   adapter: netlify({
     edgeMiddleware: true,
     cacheOnDemandPages: true,
